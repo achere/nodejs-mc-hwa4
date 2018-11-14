453d4c2665b5bad6353e0c0c9df6f3ed97916a62
Alexander Chereshnev <chethelesser@gmail.com>
2018-11-14 13:24:43 +0300

    Add entry point

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require('./lib/server');
+const cli = require('./lib/cli');
+const app = {};
+
+app.init = () => {
+  server.init();
+  cli.init();
+};
+
+app.init();
+
+module.exports = app;
